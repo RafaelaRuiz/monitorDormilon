e08ded7053f58dfc53e785f5117b542d54262931
JenniferCastrocd <1111540677@u.icesi.edu.co>
2025-03-25 11:17:26 -0500

    correccion del bug

--- src/MonitorDormilon.java
+++ src/MonitorDormilon.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-public class MonitorDormilon extends Thread{
+public class MonitorDormilon {
     public static Semaphore mutex = new Semaphore(1);
     public static Semaphore sillasDisponibles = new Semaphore(3);
     public static Semaphore monitorDisponible = new Semaphore(0);
@@ -15,18 +15,22 @@ public class MonitorDormilon extends Thread{
         Monitor monitor = new Monitor();
         monitor.start();
 
-        System.out.println("Ingrese el numero de estudiantes: ");
+        System.out.println("Ingrese el número de estudiantes: ");
         int numEstudiantes = scanner.nextInt();
         scanner.nextLine();
+
         Estudiante[] estudiantes = new Estudiante[numEstudiantes];
         for (int i = 0; i < numEstudiantes; i++) {
-            System.out.println("Ingrese el nombre del estudiante " + (i+1) + ": ");
+            System.out.println("Ingrese el nombre del estudiante " + (i + 1) + ": ");
             String nombre = scanner.nextLine();
             estudiantes[i] = new Estudiante(nombre);
         }
+
         // Iniciar los hilos de los estudiantes después de crearlos
         for (Estudiante estudiante : estudiantes) {
             estudiante.start();
         }
+
+        scanner.close();
     }
 }
